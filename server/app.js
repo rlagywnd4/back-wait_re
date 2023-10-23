cb7dec2265ce5d4014cffad543cebb5b28838974
LOBSTER10000 <ghh112034@gmail.com>
2023-10-23 19:44:06 +0900

    03folder-01 서버 연결 성공

---
+++ server/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const session = require('express-session');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+const PORT = 8000;
+
+// 익스프레스에서 json 사용하도록 해줌
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// router 설정
+const home = require('./routes/index');
+app.use('/', home);
+
+//시퀄라이즈 설정
+const { sequelize } = require('./models');
+
+sequelize
+  .sync({ force: false })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log('8000 port is running');
+    });
+  })
+  .catch(() => {
+    console.log('데이터 베이스 연결 실패');
+  });
