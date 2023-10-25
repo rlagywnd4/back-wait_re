c525cfeb2e4dff059640eb10bc9434ba15fa67d7
kdm111 <pjsu94@naver.com>
2023-10-25 16:51:07 +0900

    Feat : user update and delete

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -16,6 +16,15 @@ const inValidSignup = async (columnName, value) => {
   });
   return response ? `${columnName}값이 이미 존재합니다.` : false
 }
+const accessDecode = async (token) => {
+  await jwt.verify(token, process.env.SECRET_KEY, async (err, decoded) => {
+    const response = await User.findOne({
+      where : {id},
+      attributes : ['userId', 'email', 'nickname', 'photo', 'createdAt', 'updatedAt'],
+    })
+    return response
+  })
+}
 
 exports.register = async (req, res) => {
   try {
@@ -32,15 +41,16 @@ exports.register = async (req, res) => {
     );
     if (errMessages.length > 0) {
       res.status(400).json({ errors: errMessages });
+    } else {
+      const hashedPassword = await bcryptjs.hash(password, 10);
+      await User.create({
+        userId,
+        password : hashedPassword,
+        nickname,
+        email
+      });
+      res.status(201).send('회원 가입 완료');
     }
-    const hashedPassword = await bcryptjs.hash(password, 10);
-    await User.create({
-      userId,
-      password : hashedPassword,
-      nickname,
-      email
-    });
-    res.status(201).send('회원 가입 완료');
   } catch (err) {
     console.log(err);
     res.status(500).json({error :err.message});
@@ -61,24 +71,21 @@ exports.login = async (req, res) => {
     const isPasswordValid = await bcryptjs.compare(password, user.password);
     if (!isPasswordValid) {
       return res.status(401).json({ message: '올바르지 않은 비밀번호 입니다.' });
+    } else {
+      const userInfo = {};
+      userInfo['userId'] = user.userId;
+      userInfo['id'] = user.id;
+      const token = jwt.sign(userInfo, process.env.SECRET_KEY);
+      res.cookie('access', token, {
+        maxAge : 24 * 60 * 60 * 1000,
+        Path : '/'
+      })
+      res.status(200).json({ ...user, password : '' })
     }
-    const userInfo = {};
-    userInfo['userId'] = user.userId;
-    userInfo['id'] = user.id;
-    const token = jwt.sign(userInfo, process.env.SECRET_KEY);
-    res.cookie('access', token, {
-      maxAge : 24 * 60 * 60 * 1000,
-      Path : '/'
-    })
-    res.status(200).json({ ...user, password : '' })
   } catch (err) {
     console.log(err)
     res.status(500).json({ message: '알 수 없는 서버 에러 입니다.' })
   }
-}
-
-exports.put = (req, res) => {
-  res.send('1')
 }
 
 exports.myInfo = async (req, res) => {
@@ -86,23 +93,24 @@ exports.myInfo = async (req, res) => {
     const access = req.cookies?.access
     if (!access) {
       res.status(401).json({message : '로그인을 먼저 해주세요'})
-    }
-    await jwt.verify(access, process.env.SECRET_KEY, async (err, decoded) => {
-      if (err) {
-        res.status(403).json({message : '유효하지 않은 토큰입니다.'})
-      }
-      const id = decoded?.id
-      const response = await User.findOne({
-        where : {id},
-        attributes : ['userId', 'email', 'nickname', 'photo', 'createdAt', 'updatedAt'],
-        include : [
-          {model : Review},
-          {model : Proxy},
-          {model : WaitMate},
-        ],
+    } else {
+      await jwt.verify(access, process.env.SECRET_KEY, async (err, decoded) => {
+        if (err) {
+          res.status(403).json({message : '유효하지 않은 토큰입니다.'})
+        }
+        const id = decoded?.id
+        const response = await User.findOne({
+          where : {id},
+          attributes : ['userId', 'email', 'nickname', 'photo', 'createdAt', 'updatedAt'],
+          include : [
+            {model : Review},
+            {model : Proxy},
+            {model : WaitMate},
+          ],
+        })
+        res.send(response)
       })
-      res.send(response)
-    })
+    }
   } catch (err) {
     console.log(err)
     res.status(500).json({ message: '알 수 없는 서버 에러 입니다.' })
@@ -125,6 +133,72 @@ exports.userInfo = async (req, res) => {
       res.status(404).json({message : '존재하지 않는 사용자 입니다.'})
     } else {
       res.send(response)
+    }
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: '알 수 없는 서버 에러 입니다.' })
+  }
+}
+
+exports.updateUserInfo = async (req, res) => {
+  try {
+    const access = req.cookies?.access
+    if (!access) {
+      res.status(401).json({message : '로그인을 먼저 해주세요'})
+    }
+    if (req.body.userId) {
+      res.status(403).json({message : '아이디는 변경할 수 없습니다.'})
+    } else if (req.body.password) {
+      await jwt.verify(access, process.env.SECRET_KEY, async (err, decoded) => {
+        if (err) {
+          res.status(403).json({message : '유효하지 않은 토큰입니다.'})
+        }
+        const hashedPassword = await bcryptjs.hash(req.body.password, 10);
+        const response = await User.update({password : hashedPassword}, {
+          where : {id : decoded.id}
+        })
+        if (response) {
+          res.status(201).json({message : '업데이트가 완료되었습니다.'})
+        } else {
+          res.status(400).json({message : '이미 존재하는 값이거나 존재하지 않는 필드이름 입니다.'})
+        }
+      })
+    } else {
+      await jwt.verify(access, process.env.SECRET_KEY, async (err, decoded) => {
+        if (err) {
+          res.status(403).json({message : '유효하지 않은 토큰입니다.'})
+        }
+        const response = await User.update(req.body, {
+          where : {id : decoded.id}
+        })
+        if (response) {
+          res.status(201).json({message : '업데이트가 완료되었습니다.'})
+        } else {
+          res.status(400).json({message : '이미 존재하는 값이거나 존재하지 않는 필드이름 입니다.'})
+        }
+      })
+    }
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: '알 수 없는 서버 에러 입니다.' })
+  }
+}
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const access = req.cookies?.access
+    if (!access) {
+      res.status(401).json({message : '로그인을 먼저 해주세요'})
+    } else {
+      await jwt.verify(access, process.env.SECRET_KEY, async (err, decoded) => {
+        if (err) {
+          res.status(403).json({message : '유효하지 않은 토큰입니다.'})
+        }
+        const response = await User.destroy({
+          where : {id : decoded.id}
+        })
+        res.status(204).send()
+      })
     }
   } catch (err) {
     console.log(err)
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,14 +1,12 @@
-const express = require('express')
-const userRouter = express.Router()
-const userController = require('../controllers/userController')
+const express = require('express');
+const userRouter = express.Router();
+const userController = require('../controllers/userController');
 
+userRouter.post('/register', userController.register);
+userRouter.post('/login', userController.login);
+userRouter.get('/myinfo', userController.myInfo);
+userRouter.get('/:userId', userController.userInfo);
+userRouter.put('', userController.updateUserInfo);
+userRouter.delete('', userController.deleteUser);
 
-userRouter.post('/register', userController.register)
-userRouter.post('/login', userController.login)
-userRouter.get('/myinfo', userController.myInfo)
-userRouter.get('/:userId', userController.userInfo)
-userRouter.put('/:id', userController.put)
-
-
-
-module.exports = userRouter
+module.exports = userRouter;
