ec5af841fcefef92c24842062cd805bd060d47a0
rlagywnd4 <rlagywnd4@gmail.com>
2023-10-25 16:35:57 +0900

    feat:post.waitMate

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,13 +2,14 @@ const express = require('express');
 const { swaggerUi, specs } = require('../swagger');
 const Router = express.Router();
 const userRouter = require('./userRouter');
+const waitMateRouter = require('./waitMateRouter');
 
 Router.get('', (req, res) => {
-  res.send("hello world")
+  res.send('hello world');
 });
 
-Router.use('/user', userRouter)
+Router.use('/user', userRouter);
+Router.use('/waitMate', waitMateRouter);
 Router.use('/swagger', swaggerUi.serve, swaggerUi.setup(specs));
-
 
 module.exports = Router;
