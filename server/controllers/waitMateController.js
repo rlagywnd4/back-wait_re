c0af793718fc0de6ca4bb20bc1b1442022328a85
rlagywnd4 <rlagywnd4@gmail.com>
2023-10-25 18:09:49 +0900

    feat:get/waitMait 웨메(detail) 조회

--- server/controllers/waitMateController.js
+++ server/controllers/waitMateController.js
@@ -1,5 +1,41 @@
 const { WaitMate } = require('../models');
 
+// waitMate 조회(waitMateDetail페이지에서 사용)
+exports.getWaitMate = async (req, res) => {
+  // wmAddress를 요청에 받고 응답 값에는 id(user)를 보내 글쓴 주인인지 확인
+  try {
+    const { wmId } = req.query;
+    const waitMate = await WaitMate.findOne({
+      where: {
+        wmId,
+      },
+    });
+
+    res.json(waitMate);
+  } catch (e) {
+    console.error('Error fetching WaitMate data:', e);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+// waitMate목록 조회
+exports.getWaitMateList = async (req, res) => {
+  try {
+    const { wmAddress } = req.query;
+    console.log(req.query);
+    const waitMates = await WaitMate.findAll({
+      where: {
+        wmAddress,
+      },
+    });
+
+    res.json(waitMates);
+  } catch (e) {
+    console.error('Error fetching WaitMate data:', e);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
 // waitMate를 DB에 등록
 exports.postWaitMate = async (req, res) => {
   try {
@@ -23,23 +59,5 @@ exports.postWaitMate = async (req, res) => {
     }
   } catch (e) {
     console.log('error:', e);
-  }
-};
-
-// waitMate목록 조회
-exports.getWaitMateList = async (req, res) => {
-  try {
-    const { wmAddress } = req.query;
-    console.log(req.query);
-    const waitMates = await WaitMate.findAll({
-      where: {
-        wmAddress,
-      },
-    });
-
-    res.json(waitMates);
-  } catch (e) {
-    console.error('Error fetching WaitMate data:', e);
-    res.status(500).send('Internal Server Error');
   }
 };
--- server/routes/waitMateRouter.js
+++ server/routes/waitMateRouter.js
@@ -2,10 +2,12 @@ const express = require('express');
 const waitMateRouter = express.Router();
 const waitMateController = require('../controllers/waitMateController');
 
-// waitMate(글) 등록
-waitMateRouter.post('/', waitMateController.postWaitMate);
-
+// waitMate 조회
+waitMateRouter.get('/', waitMateController.getWaitMate);
 // waitMate 목록 조회
 waitMateRouter.get('/list', waitMateController.getWaitMateList);
+
+// waitMate(글) 등록
+waitMateRouter.post('/', waitMateController.postWaitMate);
 
 module.exports = waitMateRouter;
