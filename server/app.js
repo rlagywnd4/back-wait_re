0ec424070ae88b8f533f8d89671e07aa1d117138
junsu Park <pjsu94@naver.com>
2023-10-26 23:54:52 +0900

    Feat : Kakao로 로그인 정보 기반 회원가입 및 로그인, 공통 모듈에 cookie를 기반으로 사용자 정보를 가져오는 함수 추가

--- server/app.js
+++ server/app.js
@@ -15,7 +15,10 @@ require('dotenv').config();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true,
+}));
 
 // router 설정
 const home = require('./routes/index');
---
+++ server/common/index.js
@@ -0,0 +1,32 @@
+const jwt = require('jsonwebtoken');
+const { User, Review, Proxy, WaitMate } = require('../models');
+/**
+ * 사용자 정보를 가져오는 함수.
+ * @param {Object} req - Express의 요청 객체
+ * @returns {Promise<Object>} - 사용자 정보 객체 (Promise 반환 하므로 await를 붙이기)
+ * @throws {Error} - JWT 검증 오류 발생 시 빈 객체와 함께 에러를 던집니다.
+ * @example
+ * const Common = require('../common');
+ * const userInfo = await Common.cookieUserinfo(req);
+ */
+exports.cookieUserinfo = async (req) => {
+  const access = req?.cookies?.access;
+  return new Promise(async (resolve, reject) => {
+    await jwt.verify(access, process.env.SECRET_KEY, async (err, decoded) => {
+      if (err) {
+        return reject({})
+      }
+      const id = decoded?.id
+      const userInfo = await User.findOne({
+        where : {id},
+        attributes : ['userId', 'email', 'nickname', 'photo', 'createdAt', 'updatedAt'],
+        include : [
+          {model : Review},
+          {model : Proxy},
+          {model : WaitMate},
+        ],
+      })
+      resolve(userInfo)
+    })
+  })
+}
