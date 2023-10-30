8fa6ad0e91f2dcf0b9610fca9018a742fee5a8a7
LOBSTER10000 <ghh112034@gmail.com>
2023-10-31 00:50:58 +0900

    mongodb server연결

--- server/schema/Room.js
+++ server/schema/Room.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 
 const roomSchema = new mongoose.Schema({
+    roomNumber: {
+        type: Number,
+        required: true,
+        unique: true,
+        index: true,
+        default: 0
+    },
     wmId: {
-        type: String, // 또는 String, 데이터 유형에 따라 변경
+        type: Number, // 또는 String, 데이터 유형에 따라 변경
         required: true,
     },
     proxyId: {
-        type: String, // 또는 String, 데이터 유형에 따라 변경
+        type: Number, // 또는 String, 데이터 유형에 따라 변경
         required: true,
     },
     createdAt: {
@@ -14,6 +21,16 @@ const roomSchema = new mongoose.Schema({
         default: Date.now,
     },
 });
+
+// 방 생성 시 roomNumber 증가 메서드
+roomSchema.statics.createRoom = async function (otherFields) {
+    const Room = this;
+    const maxRoomNumber = await Room.find().sort({ roomNumber: -1 }).limit(1);
+    const newRoomNumber = maxRoomNumber.length > 0 ? maxRoomNumber[0].roomNumber + 1 : 1;
+
+    const room = new Room({ roomNumber: newRoomNumber, ...otherFields });
+    return room.save();
+};
 
 const Room = mongoose.model('Room', roomSchema);
 
