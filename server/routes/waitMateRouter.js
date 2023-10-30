8d492f9b256f6301629904ec2e87101f56e3edb0
rlagywnd4 <rlagywnd4@gmail.com>
2023-10-30 17:20:26 +0900

    fix:get요청query,응답send로 통일

--- server/routes/waitMateRouter.js
+++ server/routes/waitMateRouter.js
@@ -3,11 +3,11 @@ const waitMateRouter = express.Router();
 const waitMateController = require('../controllers/waitMateController');
 
 // waitMate 조회
-waitMateRouter.get('/', waitMateController.getWaitMate);
+waitMateRouter.get('/detail', waitMateController.getWaitMateDetail);
 // waitMate(글) 등록
 waitMateRouter.post('/', waitMateController.postWaitMate);
 // waitMate 삭제
-waitMateRouter.delete('/:wmId', waitMateController.deleteWaitMate);
+waitMateRouter.delete('/', waitMateController.deleteWaitMate);
 // waitMate 수정
 waitMateRouter.patch('/', waitMateController.patchWaitMate);
 
