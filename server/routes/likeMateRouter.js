ea3a5e8238313ec8e084d96bea06d148d47423e7
rlagywnd4 <rlagywnd4@gmail.com>
2023-10-31 20:02:01 +0900

    fix:불필요한 파일 제거

--- server/routes/likeMateRouter.js
+++
@@ -1,6 +0,0 @@
-const express = require('express');
-const likeWaitRouter = express.Router();
-const likeWaitController = require('../controllers/likeWaitController');
-
-likeWaitRouter.post('/', likeWaitController.postLikeWait);
-likeWaitRouter.delete('/', likeWaitController.deleteLikeWait);
