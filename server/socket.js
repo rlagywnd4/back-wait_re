34b3757af3da04484be0ba549db46757ad5eafee
LOBSTER10000 <ghh112034@gmail.com>
2023-11-01 18:58:14 +0900

    feat: socket: 데이터 정보값 등록 성공

--- server/socket.js
+++ server/socket.js
@@ -1,56 +1,41 @@
 
 const socketIO = require('socket.io');
-const {ChatData, Room} = require('./schema');
+const ChatData = require('./schema/ChatData');
+const Room = require('./schema/Room');
 const {ChatRoom, Proxy, WaitMate, User, Review, LikeWait} = require('./models');
 
 function setupSocket(server ) {
   const io = socketIO(server, {
     cors: {
       origin: ['http://localhost:3000'],
-      methods: ["GET","POST"],
+      methods: ["GET","POST","PATCH","DELETE"],
     }
   });
   console.log('소켓 시작');
   io.on('connection', (socket) => {
     console.log('새로운 소켓 연결이 이루어졌습니다.');
 
-    socket.on('requestChatRoom', (data) => {
-        // 웨메 및 프록시 정보 추출
-        const { wmId, proxyId } = data;
-      
-        // 방 정보를 데이터베이스에 저장 (Room 모델 사용)
-        const newRoom = new Room({ wmId, proxyId });
-        newRoom.save();
-      
-        // 웨메와 프록시를 해당 방에 연결
-        socket.join(newRoom._id);
-      
-        // 클라이언트에 채팅 방 생성 완료 메시지 보내기
-        socket.emit('chatRoomCreated', { roomId: newRoom._id });
+    socket.on('message', (data)=>{
+      console.log(data);
 
-        // 클라이언트에 채팅 생성 메세지 보내기
-        socket.to(newRoom._id).emit('AllMessage', {
-            message : '안녕하세요. 바른말 고운말만 사용해주세요~!'
+      const chatMessage = new ChatData({
+        room: data.room,
+        sender: data.sender,
+        receiver: data.receiver,
+        messageType: data.messageType,
+        messageContent: data.messageContent,
+      });
+  
+      chatMessage.save()
+        .then((savedMessage) => {
+          console.log('메시지가 성공적으로 저장되었습니다:', savedMessage);
+          // 저장 성공 시, 다른 클라이언트로 메시지를 전송할 수 있습니다.
         })
-      });
-
-      socket.on('chatting', (data) => {
-        try {
-          // 데이터베이스에 채팅 메시지 저장
-          const chatData = new ChatData({
-            room,
-            sender,
-            receiver,
-            messageType,
-            messageContent,
-          });
-          chatData.save();
-          // 채팅 메시지를 다른 클라이언트에게 보내기 (broadcast 사용)
-        socket.broadcast.emit('chatting', data);
-        } catch (error) {
-          console.error('채팅 메시지 저장 중 오류 발생: ', error);
-        }
-      });
+        .catch((error) => {
+          console.error('메시지 저장 중 오류 발생:', error);
+          // 저장 오류 시, 적절한 에러 핸들링
+        });
+    });
   });
 }
 
