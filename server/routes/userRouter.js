a404d14a8b87fcc8c8d66fdd09f58be319f0b05d
kdm111 <pjsu94@naver.com>
2023-11-01 16:36:27 +0900

    Fix : User Router 수정

--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -9,9 +9,6 @@ userRouter.post('/register', userController.register);
 userRouter.post('/login', userController.login);
 userRouter.get('/myinfo', userController.myInfo);
 userRouter.get('/kakao', userController.kakaoResult);
-userRouter.get('/kakao/data', userController.sendKakaoData);
-userRouter.get('/kakao/login', userController.kakaoUserLogin);
-userRouter.post('/img', Common.upload(`profileImg/`).single('profileImg'),  userController.uploadImage);
 userRouter.post('/check/nickname', userController.checkNickname);
 userRouter.post('/check/userId', userController.checkUserId);
 
