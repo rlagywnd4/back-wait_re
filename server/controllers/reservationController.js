45acba65b68b911574312e52a1230ed5f1feea7d
rlagywnd4 <rlagywnd4@gmail.com>
2023-11-08 19:05:29 +0900

    feat:프록시 입장에서 내가 픽한 웨메 리스트

--- server/controllers/reservationController.js
+++ server/controllers/reservationController.js
@@ -1,4 +1,5 @@
-const { WaitMate } = require('../models');
+const { WaitMate, Proxy } = require('../models');
+const Reservation = require('../models/Reservation');
 
 exports.getWaitMateReservation = async (req, res) => {
   // get/waitMate/detail과 비슷한 기능이라 필요없지만 나중에 필요할 수 있어서 남겨둠
@@ -11,6 +12,37 @@ exports.getWaitMateReservation = async (req, res) => {
     });
     if (getReservation) {
       res.send({ result: 'success', getReservation });
+    } else {
+      res.send({ result: 'fail' });
+    }
+  } catch (e) {
+    console.error('Error WaitMate data:', e);
+    res.status(500).send('Internal Server Error');
+  }
+};
+
+exports.getPickedWaitMate = async (req, res) => {
+  try {
+    const { id } = req.query;
+    let waitMateList = [];
+    const getProxyId = await Proxy.findOne({
+      where: {
+        id,
+      },
+    });
+    const getPickedWaitMate = await Reservation.findAll({
+      where: {
+        proxyId: getProxyId.proxyId,
+      },
+    });
+    getPickedWaitMate.array.forEach(async (element) => {
+      const waitMate = await WaitMate.findOne({
+        wmId: element.wmId,
+      });
+      waitMateList.push(waitMate);
+    });
+    if (getPickedWaitMate) {
+      res.send({ waitMateList: waitMateList });
     } else {
       res.send({ result: 'fail' });
     }
--- server/routes/reservationRouter.js
+++ server/routes/reservationRouter.js
@@ -2,6 +2,7 @@ const express = require('express');
 const reservationRouter = express.Router();
 const reservationController = require('../controllers/reservationController');
 
-reservationRouter.get('', reservationController.getWaitMateReservation);
+reservationRouter.get('/', reservationController.getWaitMateReservation);
+reservationRouter.get('/list', reservationController.getPickedWaitMate);
 
 module.exports = reservationRouter;
